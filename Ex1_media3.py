8b719be99c6b937f849036c42fa305d7d18e910a
marcemoromi <marcemoromi@hotmail.com>
2015-03-20 11:21:50 -0300

    Create Ex1_media3.py

---
+++ Ex1_media3.py
@@ -0,0 +1,14 @@
+#Ex1_medias3
+
+def media(a,b,c):
+    m=(a+b+c)/3
+    return m
+
+print("Calculo da media de 3 numeros:\n")
+m=0
+a=int(input("Digite o valor de a: "))
+b=int(input("Digite o valor de b: "))
+c=int(input("Digite o valor de c: "))
+m=media(a,b,c)
+print("O valor da media eh:",m)
+input()
